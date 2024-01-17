285fd637b2f13113e3893d36adcca53a3282345f
Aayu-shh <aayush28.official@gmail.com>
2024-01-17 09:25:21 +0530

    Initial Commit - Hello World

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import React from 'react' -->Not required after ES7
+
+const App = () => {
+  return (
+    <h1>
+        Hello World! Welcome to Aayush Jacked's channel.
+    </h1>
+  )
+}
+
+export default App
